294a2fc41f77fbbaef54662eda8a454f36628efe
kimjdo <kimjdo@naver.com>
2018-03-26 11:03:38 +0900

    update Views

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,8 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = User.objects.get(username='kimjdo')
+            post.author = request.user
+#            post.author = User.objects.get(username='kimjdo')
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
@@ -30,10 +31,10 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)    
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = User.objects.get(username='kimjdo')
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
